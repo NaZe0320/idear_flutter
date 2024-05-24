947b467dc66e58ab3c6fb4ce63e52da9e7baf832
NaZe0320 <rwt1226@naver.com>
2024-05-24 22:42:29 +0900

    [add] constants array 추가

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,54 @@
+class AppConstants {
+  static const List<String> relationArray = [
+    '가족',
+    '친구',
+    '동료',
+    '스승',
+    '연인',
+    '지인',
+    '제자',
+    '고객',
+    '상사',
+    '기타'
+  ];
+
+  static const List<String> privateKeywordArray = [
+    '우정',
+    '감사',
+    '입학 축하',
+    '조의',
+    '생일 축하',
+    '조언',
+    '격려',
+    '사랑',
+    '인사',
+    '기타'
+  ];
+
+  static const List<String> publicKeywordArray = [
+    '추천서',
+    '내용확인',
+    '문의 사항',
+    '조의',
+    '인사',
+    '상업적 제안서',
+    '면담 요청',
+    '상담 요청',
+    '클레임',
+    '조언',
+    '기타'
+  ];
+
+  static const List<String> moodArray = [
+    '깔끔한',
+    '다정한',
+    '냉철한',
+    '귀여운',
+    '친근한',
+    '발랄한',
+    '세련된',
+    '공손한',
+    '세심한',
+    '기타'
+  ];
+}
