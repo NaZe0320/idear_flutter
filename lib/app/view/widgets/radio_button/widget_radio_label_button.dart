59acdd70b99d26dbb0c203158f9a2ac8c150eb2a
NaZe0320 <rwt1226@naver.com>
2024-05-24 23:44:05 +0900

    [refactor] RadioButton 폴더 묶음

--- lib/app/view/widgets/radio_button/widget_radio_label_button.dart
+++ lib/app/view/widgets/radio_button/widget_radio_label_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:idear/app/view/widgets/widget_radio_button.dart';
+import 'package:idear/app/view/widgets/radio_button/widget_radio_button.dart';
 import 'package:idear/core/theme/app_colos.dart';
 import 'package:idear/core/theme/app_text_styles.dart';
 
