fb6531d50bd904852cb8f9f8b03a6043df13b3c5
NaZe0320 <rwt1226@naver.com>
2024-05-24 21:38:20 +0900

    [init] app theme setting

---
+++ lib/core/theme/app_colos.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class AppColors {
+  static const colorMain = Color(0xFF0BA7A3);
+  static const colorMainLight = Color(0xFF72C5C2);
+  static const colorMainExtraLight = Color(0xFFABDEDD);
+  static const colorSub = Color(0xFFFFD817);
+
+  static const colorWhite = Color(0xFFFFFFFF);
+  static const colorGray100 = Color(0xFFF6F6F6);
+  static const colorGray200 = Color(0xFFE8E8E8);
+  static const colorGray300 = Color(0xFFF6F6F6);
+  static const colorGray500 = Color(0xFFB7B7B7);
+  static const colorGray600 = Color(0xFF949494);
+  static const colorGray800 = Color(0xFF616161);
+  static const colorGray900 = Color(0xFF272727);
+  static const colorGBlack = Color(0xFF111111);
+}
---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:idear/core/theme/app_colos.dart';
+
+class AppTextStyles {
+  static const heading02 = TextStyle(
+      fontSize: 20, fontWeight: FontWeight.w600, color: AppColors.colorGray900);
+
+  static const subheading01 = TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w700, color: AppColors.colorGray900);
+  static const subheading02 = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w600, color: AppColors.colorGray900);
+
+  static const body01 = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.colorGray900);
+
+  static const body03 = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w600, color: AppColors.colorGray900);
+  static const body04 = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.colorGray900);
+
+  static const caption04 = TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w500, color: AppColors.colorGray500);
+}
